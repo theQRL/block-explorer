f49c835d9794f430c959a06a7ec330ebf0c280fc
JP Lomas <jp.lomas@gmail.com>
2017-02-18 13:48:58 +0000

    Initial commit

---
+++ js/formHandler.js
@@ -0,0 +1,74 @@
+$(function()
+{	
+	$("input,textarea").jqBootstrapValidation(
+    {
+     	preventSubmit: true,
+     	submitSuccess: function($form, event)
+	 	{			
+			if(!$form.attr('action')) // Check form doesnt have action attribute
+			{
+				event.preventDefault(); // prevent default submit behaviour
+			
+				var processorFile = getProcessorPath($form);
+				var formData = {};
+
+				$form.find("input, textarea, option:selected").each(function(e) // Loop over form objects build data object
+				{		
+					var fieldData =  $(this).val();
+					var fieldID =  $(this).attr('id');
+				
+					if($(this).is(':checkbox')) // Handle Checkboxes
+					{
+						fieldData = $(this).is(":checked");
+					}
+					else if($(this).is(':radio')) // Handle Radios
+					{
+						fieldData = $(this).val()+' = '+$(this).is(":checked");
+					}
+					else if($(this).is('option:selected')) // Handle Option Selects
+					{
+						fieldID = $(this).parent().attr('id');
+					}
+					
+					formData[fieldID] = fieldData;		
+				});
+	
+				$.ajax({
+		        	url: processorFile,
+		    		type: "POST",
+		    		data: formData,
+		    		cache: false,
+		    		success: function() // Success
+		 			{  
+						$form.append("<div id='form-alert'><div class='alert alert-success'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>"+$form.attr('success-msg')+"</strong></div></div>");		
+		 	   		},
+			   		error: function() // Fail
+			   		{
+						$form.append("<div id='form-alert'><div class='alert alert-danger'><button type='button' class='close' data-dismiss='alert' aria-hidden='true'>&times;</button><strong>"+$form.attr('fail-msg')+"</strong></div></div>");	
+			   		},
+					complete: function() // Clear
+					{
+						$form.trigger("reset");
+					},
+		   		});
+			}
+         },
+         filter: function() // Handle hidden form elements
+		 {
+			 return $(this).is(":visible");
+         },
+	 });
+	 
+	 // Get Path to processor PHP file
+	 function getProcessorPath(form)
+	 {
+		var path = "./includes/"+form.attr('id')+".php";
+		
+		if(form.attr('template-path')) // Check For Template path
+		{
+			path = form.attr('template-path')+"/includes/"+form.attr('id')+".php";
+		}
+		
+	 	return path
+	 }
+});
