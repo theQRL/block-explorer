5981a4a34cd533c184ef58f56a47574336705215
JP Lomas <jp.lomas@gmail.com>
2017-08-22 22:46:05 +0100

    Add momentjs & uptime formatted appropriately

--- imports/ui/components/status/status.js
+++ imports/ui/components/status/status.js
@@ -15,6 +15,11 @@ Template.status.helpers({
     status() {
       return Session.get("status");
     },
+    uptime() {
+      var x = Session.get("status");
+      x = x.network_uptime;
+      return moment("1900-01-01 00:00:00").add(x, 'seconds').format("d[d] h[h] mm[min]");
+    }
 });
 
 Template.status.events({
