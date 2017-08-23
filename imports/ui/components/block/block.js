0c4520aa6774fe705731b25e338a27db38c1663c
JP Lomas <jp.lomas@gmail.com>
2017-08-23 15:31:32 +0100

    Block page initial UI added

--- imports/ui/components/block/block.js
+++ imports/ui/components/block/block.js
@@ -19,7 +19,15 @@ Template.block.helpers({
     },
     blockdata() {
       return JSON.stringify(Session.get("block"), true, 2);
-    }
+    },
+    ts() {
+      try {
+     var x = moment.unix(this.blockheader.timestamp);
+     return moment(x).format("HH:mm D MMM YYYY");
+   } catch (e) {
+    return " ";
+   }
+    },
 });
 
 Template.block.events({
