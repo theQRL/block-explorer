077b095b2464ec1e5e8de84d0ba314aa7c495c31
JP Lomas <jp.lomas@gmail.com>
2017-11-14 16:39:18 +0000

    getStats and getObject tests

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,3 +1,20 @@
 // Import client startup through a single index entry point
 
 import './routes.js'
+
+Meteor.call('getStats', (err, res) => {
+  if (err) {
+    console.log(err.message)
+  } else {
+    console.log(res)
+  }
+})
+
+const req = { block: 444 }
+Meteor.call('getObject', req, (err, res) => {
+  if (err) {
+    console.log(err.message)
+  } else {
+    console.log(res)
+  }
+})
