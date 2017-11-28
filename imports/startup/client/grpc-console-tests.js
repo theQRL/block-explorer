906930470785c2cdf178d953242738c7ecccde46
JP Lomas <jp.lomas@gmail.com>
2017-11-28 21:21:31 +0000

    Console test tidy

--- imports/startup/client/grpc-console-tests.js
+++ imports/startup/client/grpc-console-tests.js
@@ -1,5 +1,5 @@
 /* eslint no-console: 0 */
-const ab2str = buf => String.fromCharCode.apply(null, new Uint16Array(buf))
+// const ab2str = buf => String.fromCharCode.apply(null, new Uint16Array(buf))
 
 Meteor.call('getStats', (err, res) => {
   if (err) {
@@ -12,7 +12,6 @@ Meteor.call('getStats', (err, res) => {
 let req = {
   query: Buffer.from(('63e4fc9803fb0c44d98dbff04f54ca2592e4faa9964bac2ed4f5715fc753c54a').toString()),
 }
-console.log(req)
 Meteor.call('getObject', req, (err, res) => {
   if (err) {
     console.log(err.message)
@@ -20,50 +19,63 @@ Meteor.call('getObject', req, (err, res) => {
     console.log(res)
   }
 })
+
 req = {
   query: Buffer.from(('1').toString()),
 }
-console.log(req)
 Meteor.call('getObject', req, (err, res) => {
   if (err) {
+    console.log('GET BLOCK 1: - **ERROR**')
     console.log(err.message)
   } else {
+    console.log('GET BLOCK 1:')
     console.log(res)
   }
 })
+
 
 req = { filter: 'BLOCKHEADERS', offset: 0, quantity: 5 }
 Meteor.call('getLatestData', req, (err, res) => {
   if (err) {
+    console.log('LATEST BLOCKHEADERS: - **ERROR**')
     console.log(err.message)
   } else {
+    console.log('LATEST BLOCKHEADERS:')
     console.log(res)
   }
 })
 
+
 req = { filter: 'TRANSACTIONS', offset: 0, quantity: 5 }
 Meteor.call('getLatestData', req, (err, res) => {
   if (err) {
+    console.log('LATEST TRANSACTIONS: - **ERROR**')
     console.log(err.message)
   } else {
+    console.log('LATEST TRANSACTIONS:')
     console.log(res)
   }
 })
+
 
 req = {
   address: Buffer.from('Qa02d909723512ecd1606c96f52f5a4121946f068986e612a57c75353952ab3624ddd0bd6', 'ascii'),
 }
 Meteor.call('getAddressState', req, (err, res) => {
   if (err) {
+    console.log('ADDRESS STATE: - **ERROR**')
     console.log(err.message)
   } else {
+    console.log('ADDRESS STATE:')
     console.log(res)
   }
 })
 
+
 req = { filter: 'CURRENT', offset: 0, quantity: 5 }
 Meteor.call('stakers', req, (err, res) => {
   if (err) {
+    console.log('CURRENT STAKERS: - **ERROR**')
     console.log(err.message)
   } else {
     console.log('CURRENT STAKERS:')
@@ -71,9 +83,11 @@ Meteor.call('stakers', req, (err, res) => {
   }
 })
 
+
 req = { filter: 'NEXT', offset: 0, quantity: 5 }
 Meteor.call('stakers', req, (err, res) => {
   if (err) {
+    console.log('NEXT STAKERS: - **ERROR**')
     console.log(err.message)
   } else {
     console.log('NEXT STAKERS:')
