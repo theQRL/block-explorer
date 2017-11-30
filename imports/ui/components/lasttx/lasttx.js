747b945355354e0ebb7e7e96186776f34755d6a7
Juan Leni <lenijuan@gmail.com>
2017-11-30 06:22:35 +0100

    Adding support for transactionextended objects

--- imports/ui/components/lasttx/lasttx.js
+++ imports/ui/components/lasttx/lasttx.js
@@ -7,14 +7,6 @@ Template.lasttx.onCreated(() => {
     if (err) {
       Session.set('lasttx', { error: err })
     } else {
-      res.transactions = res.transactions.reverse()
-      const tx = []
-      res.transactions.forEach((index) => {
-        const obj = index
-        obj.amount = parseFloat(obj.amount)
-        tx.push(obj)
-      })
-      res.transactions = tx
       Session.set('lasttx', res)
     }
   })
@@ -24,17 +16,35 @@ Template.lasttx.helpers({
   lasttx() {
     return Session.get('lasttx')
   },
+  amount()
+  {
+    if(this.tx.coinbase)
+    {
+      // FIXME: We need a unified way to format Quantas
+      return this.tx.coinbase.amount * 1e-8
+    }
+    if(this.tx.transfer)
+    {
+      // FIXME: We need a unified way to format Quantas
+      return this.tx.transfer.amount * 1e-8
+    }
+    return ''
+  },
+  tx_hash()
+  {
+    return Buffer.from(this.tx.transaction_hash).toString('hex')
+  },
+  block()
+  {
+    return this.header.block_number;
+  },
   ts() {
-    const x = moment.unix(this.timestamp)
+    const x = moment.unix(this.header.timestamp.seconds)
     return moment(x).format('HH:mm D MMM YYYY')
   },
   zeroCheck() {
     const x = Session.get('lasttx')
-    let y = true
-    if (x.length > 0) {
-      y = false
-    }
-    return y
+    return x.length === 0
   },
 })
 
--- imports/ui/components/tx/tx.js
+++ imports/ui/components/tx/tx.js
@@ -12,31 +12,29 @@ Template.tx.onCreated(() => {
       if (err) {
         Session.set('txhash', { error: err, id: txId })
       } else {
-
         // TODO: This could probably be unified with block
-        res.transaction.amount = parseFloat(res.transaction.amount)
-        res.transaction.addr_from = Buffer.from(res.transaction.addr_from).toString()
-        res.transaction.transaction_hash = Buffer.from(res.transaction.transaction_hash).toString('hex')
+        res.transaction.tx.addr_from = Buffer.from(res.transaction.tx.addr_from).toString()
+        res.transaction.tx.transaction_hash = Buffer.from(res.transaction.tx.transaction_hash).toString('hex')
 
-        res.transaction.addr_from = Buffer.from(res.transaction.addr_from).toString()
-
-        res.transaction.addr_to = ''
-        res.transaction.amount = ''
+        res.transaction.tx.addr_to = ''
+        res.transaction.tx.amount = ''
         if(res.transaction.coinbase)
         {
-          res.transaction.addr_to = Buffer.from(res.transaction.coinbase.addr_to).toString()
-          // FIXME: We need a unified way to format Quantas
-          res.transaction.amount = res.transaction.coinbase.amount * 1e-8
+          res.transaction.tx.addr_to = Buffer.from(res.transaction.tx.coinbase.addr_to).toString()
+          res.transaction.tx.coinbase.addr_to = Buffer.from(res.transaction.tx.coinbase.addr_to).toString()
+          // FIXME: We need a unified way to format Quanta
+          res.transaction.tx.amount = res.transaction.tx.coinbase.amount * 1e-8
         }
-        if(res.transaction.transfer)
+        if(res.transaction.tx.transfer)
         {
-          res.transaction.addr_to = Buffer.from(res.transaction.transfer.addr_to).toString()
-          // FIXME: We need a unified way to format Quantas
-          res.transaction.amount = res.transaction.transfer.amount * 1e-8
+          res.transaction.tx.addr_to = Buffer.from(res.transaction.tx.transfer.addr_to).toString()
+          res.transaction.tx.transfer.addr_to = Buffer.from(res.transaction.tx.transfer.addr_to).toString()
+          // FIXME: We need a unified way to format Quanta
+          res.transaction.tx.amount = res.transaction.tx.transfer.amount * 1e-8
         }
 
-        res.transaction.public_key = Buffer.from(res.transaction.public_key).toString('hex')
-        res.transaction.signature = Buffer.from(res.transaction.signature).toString('hex')
+        res.transaction.tx.public_key = Buffer.from(res.transaction.tx.public_key).toString('hex')
+        res.transaction.tx.signature = Buffer.from(res.transaction.tx.signature).toString('hex')
 
         Session.set('txhash', res)
       }
@@ -55,6 +53,9 @@ Template.tx.helpers({
   tx() {
     return Session.get('txhash').transaction
   },
+  header() {
+    return Session.get('txhash').transaction.header
+  },
   qrl() {
     const txhash = Session.get('txhash')
     try {
@@ -65,25 +66,32 @@ Template.tx.helpers({
       return 0
     }
   },
+  confirmations() {
+    const x = Session.get('status')
+    try {
+      return x.node_info.block_height - this.header.block_number
+    } catch (e) { }
+    return 0
+  },
   ts() {
-    const x = moment.unix(this.timestamp)
+    const x = moment.unix(this.header.timestamp.seconds)
     return moment(x).format('HH:mm D MMM YYYY')
   },
   color() {
-    if (this.transactionType === 'coinbase') {
+    if (this.tx.transactionType === 'coinbase') {
       return 'teal'
     }
-    if (this.transactionType === 'stake') {
+    if (this.tx.transactionType === 'stake') {
       return 'red'
     }
-    if (this.transactionType === 'transfer') {
+    if (this.tx.transactionType === 'transfer') {
       return 'yellow'
     }
     return ''
   },
   json() {
     // TODO: Improve the formatting here
-    const myJSON = JSON.stringify(this, null, 4)
+    const myJSON = JSON.stringify(this.tx, null, 4)
     return myJSON.replace(new RegExp("\\\\n", "g"), "<br />");
   },
 })
