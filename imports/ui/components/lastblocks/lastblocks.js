a94a0500406a72eb419f8f1383d6a8eded343e2b
JP Lomas <jp.lomas@gmail.com>
2017-12-02 21:18:52 +0000

    add BH

--- imports/ui/components/lastblocks/lastblocks.js
+++ imports/ui/components/lastblocks/lastblocks.js
@@ -16,6 +16,7 @@ const renderLastBlocksBlock = () => {
       res.blockheaders.forEach((bh) => {
         editedBlockheaders.push(addHex(bh))
       })
+      res.blockheaders = editedBlockheaders
       Session.set('lastblocks', res)
     }
   })
