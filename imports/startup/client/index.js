b5cc1ceb723c9a8b909d8519f90190ff12c572dd
JP Lomas <jp.lomas@gmail.com>
2017-12-18 15:09:40 +0000

    Disable console tests

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,4 +1,6 @@
 // Import client startup through a single index entry point
 
 import './routes.js'
-import './grpc-console-tests.js'
+
+// a few client console test gRPC calls
+// import './grpc-console-tests.js'
