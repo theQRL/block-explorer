3c87faecf094c4fcd9660c117621760b40e8889f
JP Lomas <jp.lomas@gmail.com>
2017-12-19 01:19:17 +0000

    Fix for reversed latest transactions on refresh
    Thanks for the bug report Scott

--- imports/ui/components/lasttx/lasttx.js
+++ imports/ui/components/lasttx/lasttx.js
@@ -1,42 +1,48 @@
 import './lasttx.html'
 
-Template.lasttx.onCreated(() => {
-  Session.set('lasttx', {})
+const renderLastTxBlock = () => {
   Meteor.call('lasttx', (err, res) => {
     // The method call sets the Session variable to the callback value
     if (err) {
       Session.set('lasttx', { error: err })
     } else {
+      const tx = []
+      res.transactions.forEach((index) => {
+        const obj = index
+        obj.amount = parseFloat(obj.amount)
+        tx.push(obj)
+      })
+      res.transactions = tx
       Session.set('lasttx', res)
     }
   })
+}
+
+Template.lasttx.onCreated(() => {
+  Session.set('lasttx', {})
+  renderLastTxBlock()
 })
 
 Template.lasttx.helpers({
   lasttx() {
     return Session.get('lasttx')
   },
-  amount()
-  {
-    if(this.tx.coinbase)
-    {
+  amount() {
+    if (this.tx.coinbase) {
       // FIXME: We need a unified way to format Quantas
       return this.tx.coinbase.amount * 1e-8
     }
-    if(this.tx.transfer)
-    {
+    if (this.tx.transfer) {
       // FIXME: We need a unified way to format Quantas
       return this.tx.transfer.amount * 1e-8
     }
     return ''
   },
-  tx_hash()
-  {
+  tx_hash() {
     return Buffer.from(this.tx.transaction_hash).toString('hex')
   },
-  block()
-  {
-    return this.header.block_number;
+  block() {
+    return this.header.block_number
   },
   ts() {
     const x = moment.unix(this.header.timestamp.seconds)
@@ -50,23 +56,7 @@ Template.lasttx.helpers({
 
 Template.lasttx.events({
   'click .refresh': () => {
-    Session.set('lasttx', {})
-    Meteor.call('lasttx', (err, res) => {
-      // The method call sets the Session variable to the callback value
-      if (err) {
-        Session.set('lasttx', { error: err })
-      } else {
-        res.transactions = res.transactions.reverse()
-        const tx = []
-        res.transactions.forEach((index) => {
-          const obj = index
-          obj.amount = parseFloat(obj.amount)
-          tx.push(obj)
-        })
-        res.transactions = tx
-        Session.set('lasttx', res)
-      }
-    })
+
   },
   'click .close': () => {
     $('.message').hide()
