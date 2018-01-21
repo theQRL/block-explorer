d26509ea1bb910ac7d78ded4a48e8ff6dd6ceaed
JP Lomas <jp.lomas@gmail.com>
2018-01-21 20:01:13 +0000

    New block check refresh hammer 3s -> 30s

--- imports/startup/server/cron.js
+++ imports/startup/server/cron.js
@@ -1,11 +1,9 @@
 import { getLatestData } from '/imports/startup/server/index.js'
-
 import { Blocks } from '/imports/api/index.js'
 
-/* eslint no-console:0 */
 Meteor.setInterval(() => {
   const request = { filter: 'BLOCKHEADERS', offset: 0, quantity: 5 }
   const response = Meteor.wrapAsync(getLatestData)(request)
   Blocks.remove({})
   Blocks.insert(response)
-}, 3000)
+}, 30000)
