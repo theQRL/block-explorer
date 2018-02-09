0d323d9fa55f559f84a64d5e522204dad335d2bf
Scott Donald <scottdonald@protonmail.ch>
2018-02-10 08:12:34 +1100

    Start reskinning

--- imports/ui/pages/home/home.js
+++ imports/ui/pages/home/home.js
@@ -12,57 +12,112 @@ Template.appHome.onRendered(() => {
   })
   $('#chart').parent().height(h)
 
-  const options = {
-    // Boolean - Whether grid lines are shown across the chart
-    scaleShowGridLines: true,
-    // String - Colour of the grid lines
-    scaleGridLineColor: 'rgba(0,0,0,.05)',
-    // Number - Width of the grid lines
-    scaleGridLineWidth: 1,
-    // Boolean - Whether to show horizontal lines (except X axis)
-    scaleShowHorizontalLines: true,
-    // Boolean - Whether to show vertical lines (except Y axis)
-    scaleShowVerticalLines: true,
-    // Boolean - Whether the line is curved between points
-    bezierCurve: true,
-    // Number - Tension of the bezier curve between points
-    bezierCurveTension: 0.4,
-    // Boolean - Whether to show a dot for each point
-    pointDot: true,
-    // Number - Radius of each point dot in pixels
-    pointDotRadius: 4,
-    // Number - Pixel width of point dot stroke
-    pointDotStrokeWidth: 1,
-    // Number - amount extra to add to the radius to cater for hit detection outside the drawn point
-    pointHitDetectionRadius: 20,
-    // Boolean - Whether to show a stroke for datasets
-    datasetStroke: true,
-    // Number - Pixel width of dataset stroke
-    datasetStrokeWidth: 2,
-    // Boolean - Whether to fill the dataset with a colour
-    datasetFill: true,
-    // String - A legend template
-    legendTemplate: '<ul class="<%=name.toLowerCase()%>-legend""><% for (var i=0; i<datasets.length; i++){%><li><span style="background-color:<%=datasets[i].strokeColor%>"></span><%if(datasets[i].label){%><%=datasets[i].label%><%}%></li><%}%></ul>',
-    // Boolean - whether or not the chart should be responsive and resize when the browser does.
-    responsive: true,
-    // Boolean - whether to maintain the starting aspect ratio or not when responsive, if set to false, will take up entire container
-    maintainAspectRatio: false,
-  }
-  const data = {
-    labels: ['January', 'February', 'March', 'April', 'May', 'June', 'July'],
-    datasets: [{
-      label: 'My First dataset',
-      fillColor: 'rgba(220,220,220,0.2)',
-      strokeColor: '#1d2951',
-      pointColor: '#1d2951',
-      pointStrokeColor: '#fff',
-      pointHighlightFill: 'rgb(165,11,94)',
-      pointHighlightStroke: '#bbb',
-      data: [25, 36, 56, 100, 128, 177, 180],
-    }],
-  }
+ 
+  // Draw chart on homepage
+  Meteor.call('getStats', { include_timeseries: true }, (err, res) => {
+    if (err) {
+      console.log(err)
+    } else {
+      console.log('stat')
+      console.log(res)
 
-//  const ctx = document.getElementById('myChart').getContext('2d')
-//  const myLineChart = new Chart(ctx).Line(data, options) /* eslint no-undef:0, no-unused-vars:0 */
+      let chartLineData = {
+        labels: [],
+        datasets: [],
+      }
 
+      // Build Difficulty and hash power datasets and Block # labels
+      let labels = []
+      let hashPower = {
+        label: 'Hash Power',
+        borderColor: '#DC255D',
+        backgroundColor: '#DC255D',
+        fill: false,
+        data: [],
+        yAxisID: "y-axis-2",
+        pointRadius: 0,
+        borderWidth: 2,
+      }
+      let difficulty = {
+        label: 'Difficulty',
+        borderColor: '#4A90E2',
+        backgroundColor: '#4A90E2',
+        fill: false,
+        data: [],
+        yAxisID: "y-axis-2",
+        pointRadius: 0,
+        borderWidth: 2,
+      }
+      let movingAverage = {
+        label: 'Block Time Average',
+        borderColor: '#0A0724',
+        backgroundColor: '#0A0724',
+        fill: false,
+        data: [],
+        yAxisID: "y-axis-1",
+        pointRadius: 0,
+        borderWidth: 2,
+      }
+      let blockTime = {
+        label: 'Block Time',
+        borderColor: '#1EE9CB',
+        backgroundColor: '#1EE9CB',
+        fill: false,
+        showLine: false,
+        data: [],
+        yAxisID: "y-axis-1",
+        pointRadius: 2,
+        borderWidth: 2,
+      }
+
+
+      _.each(res.block_timeseries, (entry) => {
+        labels.push(entry.number)
+        hashPower.data.push(entry.hash_power)
+        difficulty.data.push(entry.difficulty)
+        movingAverage.data.push(entry.time_movavg)
+        blockTime.data.push(entry.time_last)
+      })
+
+      chartLineData.labels = labels
+      chartLineData.datasets.push(hashPower)
+      chartLineData.datasets.push(difficulty)
+      chartLineData.datasets.push(movingAverage)
+      chartLineData.datasets.push(blockTime)
+
+      const ctx = document.getElementById('myChart').getContext('2d')
+
+      let myChart = new Chart(ctx, {
+        type: 'line',
+        data: chartLineData,
+        options: {
+          responsive: true,
+          hoverMode: 'index',
+          stacked: false,
+          scales: {
+            yAxes: [
+              {
+                type: "linear",
+                display: true,
+                position: "left",
+                id: "y-axis-1",
+                ticks: {
+                  beginAtZero: true,
+                  max: 150
+                }
+              },{
+                type: "linear",
+                display: true,
+                position: "right",
+                id: "y-axis-2",
+                gridLines: {
+                  drawOnChartArea: false,
+                },
+              }
+            ],
+          }
+        }
+      }) /* eslint no-undef:0, no-unused-vars:0 */
+    }
+  })
 })
