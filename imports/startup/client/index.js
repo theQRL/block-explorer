3852723c6d0c84669f2a37526a71add86615b8c2
JP Lomas <jp.lomas@gmail.com>
2018-05-29 15:49:50 +0100

    Update developer note

--- imports/startup/client/index.js
+++ imports/startup/client/index.js
@@ -1,11 +1,15 @@
 // Import client startup through a single index entry point
 import './routes.js'
+import { EXPLORER_VERSION } from '../both/index.js'
 
 // disable hot code push
 Meteor._reload.onMigrate(() => [false]) /* eslint no-underscore-dangle: 0 */
 
-// a few client console test gRPC calls
-// import './grpc-console-tests.js'
+// Developer note
+console.log('block-explorer - ', EXPLORER_VERSION) /* eslint no-console: 0 */
+console.log('We\'re hiring! Tell us about yourself at jobs@theqrl.org') /* eslint no-console: 0 */
+console.log('Found a security bug? security@theqrl.org') /* eslint no-console: 0 */
+console.log('Found a problem? https://github.com/theQRL/block-explorer/issues') /* eslint no-console: 0 */
 
 // Convert bytes to hex
 export function bytesToHex(byteArray) {
