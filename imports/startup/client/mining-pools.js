d5fef05d0b759bfa358e309495a6f1caaa836a65
JP Lomas <jp.lomas@gmail.com>
2018-07-08 22:37:56 +0100

    Single quotes in mining pool strings

--- imports/startup/client/mining-pools.js
+++ imports/startup/client/mining-pools.js
@@ -1,14 +1,14 @@
 export const MINING_POOLS = [
   {
-    address: "Q010900411c50a98e65d289b51d9d4dc0f61833c26f2c6c4f3ce4df8e9968d1fcae6f6341392bea",
-    email: "support@qrlpool.org",
-    link: "https://qrlpool.org",
-    name: "qrlpool.org",
+    address: 'Q010900411c50a98e65d289b51d9d4dc0f61833c26f2c6c4f3ce4df8e9968d1fcae6f6341392bea',
+    email: 'support@qrlpool.org',
+    link: 'https://qrlpool.org',
+    name: 'qrlpool.org',
   },
   {
-    address: "Q010600286a4c7bcc7f701dc7cf0389fd9be402b610894e306aad35078539599398f9681c64e56c",
-    email: "qrlmining@gmail.com",
-    link: "https://QRLmining.info",
-    name: "qrlmining.info",
+    address: 'Q010600286a4c7bcc7f701dc7cf0389fd9be402b610894e306aad35078539599398f9681c64e56c',
+    email: 'qrlmining@gmail.com',
+    link: 'https://QRLmining.info',
+    name: 'qrlmining.info',
   },
 ]
