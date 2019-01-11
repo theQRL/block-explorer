c9df76d44cc4150aae1c84e37d9a7258da564bcd
JP Lomas <jp.lomas@gmail.com>
2019-01-11 19:43:20 +0000

    Theme toggle UI visual tweak

--- imports/ui/mobile/mobile.js
+++ imports/ui/mobile/mobile.js
@@ -28,17 +28,21 @@ Template.mobile.events({
       if (x === 'dark') {
         LocalStore.set('theme', 'light')
         updateStyleSheet('light')
+        $('.sun').addClass('moon').removeClass('sun')
       } else {
         LocalStore.set('theme', 'dark')
         updateStyleSheet('dark')
+        $('.moon').addClass('sun').removeClass('moon')
       }
     } catch (e) {
       // localstore not supported so work out what theme is in use and switch accordingly
       if ($('.main-content').css('background-image').indexOf('dark') > 0) {
         // light theme in use
         updateStyleSheet('dark')
+        $('.moon').addClass('sun').removeClass('moon')
       } else {
         updateStyleSheet('light')
+        $('.sun').addClass('moon').removeClass('sun')
       }
     }
   },
@@ -53,6 +57,22 @@ Template.mobile.helpers({
       return '/img/qrl-logo-dark.png'
     }
     return '/img/qrl-logo-white.png'
+  },
+  theme() {
+    try {
+      const x = LocalStore.get('theme')
+      if (x === 'dark') {
+        return 'sun'
+      }
+      return 'moon'
+    } catch (e) {
+      // localstore not supported so work out what theme is in use and switch accordingly
+      if ($('.main-content').css('background-image').indexOf('dark') > 0) {
+        // light theme in use
+        return 'moon'
+      }
+      return 'sun'
+    }
   },
   menuBlocksActive() {
     if (
