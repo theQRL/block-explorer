a481a7e5d7029823033c6db341908a20787727a8
JP Lomas <jp.lomas@gmail.com>
2019-03-10 22:43:30 +0000

    Fix address console errors during load

--- imports/client/ui/address.js
+++ imports/client/ui/address.js
@@ -78,73 +78,90 @@ Template.address.helpers({
       const newOtsBitfield = {}
       let lowestUnusedOtsKey = -1
       let otsBitfieldLength = 0
+      let thisOtsBitfield = false
+      try { thisOtsBitfield = Session.get('address').ots_bit_field } catch (e) { return false }
+      if (thisOtsBitfield !== undefined) {
+        thisOtsBitfield.forEach((item, index) => {
+          const thisDecimal = new Uint8Array(item)[0]
+          const thisBinary = decimalToBinary(thisDecimal).reverse()
+          const startIndex = index * 8
 
-      const thisOtsBitfield = Session.get('address').ots_bit_field
-      thisOtsBitfield.forEach((item, index) => {
-        const thisDecimal = new Uint8Array(item)[0]
-        const thisBinary = decimalToBinary(thisDecimal).reverse()
-        const startIndex = index * 8
+          for (let i = 0; i < 8; i += 1) {
+            const thisOtsIndex = startIndex + i
 
-        for (let i = 0; i < 8; i += 1) {
-          const thisOtsIndex = startIndex + i
+            // Add to parsed array
+            newOtsBitfield[thisOtsIndex] = thisBinary[i]
 
-          // Add to parsed array
-          newOtsBitfield[thisOtsIndex] = thisBinary[i]
+            // Check if this is lowest unused key
+            if ((thisBinary[i] === 0) && ((thisOtsIndex < lowestUnusedOtsKey) || (lowestUnusedOtsKey === -1))) {
+              lowestUnusedOtsKey = thisOtsIndex
+            }
 
-          // Check if this is lowest unused key
-          if ((thisBinary[i] === 0) && ((thisOtsIndex < lowestUnusedOtsKey) || (lowestUnusedOtsKey === -1))) {
-            lowestUnusedOtsKey = thisOtsIndex
+            // Increment otsBitfieldLength
+            otsBitfieldLength += 1
           }
+        })
 
-          // Increment otsBitfieldLength
-          otsBitfieldLength += 1
+        // If all keys in bitfield are used, lowest key will be what is shown in ots_counter + 1
+        if (lowestUnusedOtsKey === -1) {
+          if (Session.get('address').ots_counter === '0') {
+            lowestUnusedOtsKey = otsBitfieldLength
+          } else {
+            lowestUnusedOtsKey = parseInt(Session.get('address').ots_counter, 10) + 1
+          }
         }
-      })
 
-      // If all keys in bitfield are used, lowest key will be what is shown in ots_counter + 1
-      if (lowestUnusedOtsKey === -1) {
-        if (Session.get('address').ots_counter === '0') {
-          lowestUnusedOtsKey = otsBitfieldLength
-        } else {
-          lowestUnusedOtsKey = parseInt(Session.get('address').ots_counter, 10) + 1
+        // Calculate number of keys that are consumed
+        let totalKeysConsumed = 0
+        // First add all tracked keys from bitfield
+        for (let i = 0; i < otsBitfieldLength; i += 1) {
+          if (newOtsBitfield[i] === 1) {
+            totalKeysConsumed += 1
+          }
         }
-      }
 
-      // Calculate number of keys that are consumed
-      let totalKeysConsumed = 0
-      // First add all tracked keys from bitfield
-      for (let i = 0; i < otsBitfieldLength; i += 1) {
-        if (newOtsBitfield[i] === 1) {
-          totalKeysConsumed += 1
+        // Then add any extra from `otsBitfieldLength` to `ots_counter`
+        if (Session.get('address').ots_counter !== 0) {
+          totalKeysConsumed += parseInt(Session.get('address').ots_counter, 10) - (otsBitfieldLength - 1)
         }
+
+        // Add in OTS fields to response
+        // response.ots = {}
+        // response.ots.keys = newOtsBitfield
+        // response.ots.nextKey = lowestUnusedOtsKey
+        // response.ots.keysConsumed = totalKeysConsumed
+
+        // console.log(newOtsBitfield)
+        // console.log(totalKeysConsumed)
+        // console.log(lowestUnusedOtsKey)
+
+        result.keysRemaining = result.totalSignatures - totalKeysConsumed
       }
-
-      // Then add any extra from `otsBitfieldLength` to `ots_counter`
-      if (Session.get('address').ots_counter !== 0) {
-        totalKeysConsumed += parseInt(Session.get('address').ots_counter, 10) - (otsBitfieldLength - 1)
-      }
-
-      // Add in OTS fields to response
-      // response.ots = {}
-      // response.ots.keys = newOtsBitfield
-      // response.ots.nextKey = lowestUnusedOtsKey
-      // response.ots.keysConsumed = totalKeysConsumed
-
-      // console.log(newOtsBitfield)
-      // console.log(totalKeysConsumed)
-      // console.log(lowestUnusedOtsKey)
-
-      result.keysRemaining = result.totalSignatures - totalKeysConsumed
     }
     result.signatureScheme = validationResult.sig.type
     result.hashFunction = validationResult.hash.function
     result.valid = validationResult.result
     result.validator = validationResult
-    if (result.validator.sig.result === true) { result.validator.sig.class = 'fa-check text-success' } else { result.validator.sig.class = 'fa-times text-danger' }
-    if (result.validator.len.result === true) { result.validator.len.class = 'fa-check text-success' } else { result.validator.len.class = 'fa-times text-danger' }
-    if (result.validator.hash.result === true) { result.validator.hash.class = 'fa-check text-success' } else { result.validator.hash.class = 'fa-times text-danger' }
-    if (result.validator.checksum.result === true) { result.validator.checksum.class = 'fa-check text-success' } else { result.validator.checksum.class = 'fa-times text-danger' }
-    console.log(validationResult)
+    if (result.validator.sig.result === true) {
+      result.validator.sig.class = 'fa-check text-success'
+    } else {
+      result.validator.sig.class = 'fa-times text-danger'
+    }
+    if (result.validator.len.result === true) {
+      result.validator.len.class = 'fa-check text-success'
+    } else {
+      result.validator.len.class = 'fa-times text-danger'
+    }
+    if (result.validator.hash.result === true) {
+      result.validator.hash.class = 'fa-check text-success'
+    } else {
+      result.validator.hash.class = 'fa-times text-danger'
+    }
+    if (result.validator.checksum.result === true) {
+      result.validator.checksum.class = 'fa-check text-success'
+    } else {
+      result.validator.checksum.class = 'fa-times text-danger'
+    }
     return result
   },
 })
